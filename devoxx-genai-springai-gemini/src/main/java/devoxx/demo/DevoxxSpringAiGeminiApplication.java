9039a6d305a4b5d0eb2393bf526cf1b0e2a6f7c3
Cedrick Lunven <cedrick.lunven@gmail.com>
2024-04-13 12:03:52 +0200

    more samples

---
+++ devoxx-genai-springai-gemini/src/main/java/devoxx/demo/DevoxxSpringAiGeminiApplication.java
@@ -0,0 +1,13 @@
+package devoxx.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevoxxSpringAiGeminiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevoxxSpringAiGeminiApplication.class, args);
+	}
+
+}
